d77833a2a8f6a164903f1af25e630815fb2ae54f
cnd86038-ai <cnd86038@email.vccs.edu>
2026-02-11 12:46:21 -0500

    Added cout display information distance traveled in town + distance traveled on the highway.

--- Program- Distance Per Tank of Gas/main.cpp
+++ Program- Distance Per Tank of Gas/main.cpp
@@ -3,10 +3,14 @@ using namespace std;
 
 int main() 
 {
+	{
 	int TANK_SIZE = 20;
 	int MPG_TOWN = 23.5;
 	int MPG_HIGHWAY = 28.9;
 
 	int distance_town = TANK_SIZE * MPG_TOWN;
 	int distance_highway = TANK_SIZE * MPG_HIGHWAY;
+	{	cout << "Distance per tank of gas in town: " << distance_town << " miles" << endl;
+		cout << "Distance per tank of gas on the highway: " << distance_highway << " miles" << endl;
+	}
 }
