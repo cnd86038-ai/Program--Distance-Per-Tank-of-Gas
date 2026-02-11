2a60c6315399454760056a62110b7c69d64b6c7d
cnd86038-ai <cnd86038@email.vccs.edu>
2026-02-11 13:34:48 -0500

    Fixed the issue with the data type being "int" and rounding all my numbers. Altered to be "const double" and changed 20 to a decimal to be properly ran.

--- Program- Distance Per Tank of Gas/main.cpp
+++ Program- Distance Per Tank of Gas/main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() 
 {
 
-	int TANK_SIZE = 20;
-	int MPG_TOWN = 23.5;
-	int MPG_HIGHWAY = 28.9;
+	const double TANK_SIZE = 20.0;
+	const double MPG_TOWN = 23.5;
+	const double MPG_HIGHWAY = 28.9;
 
 	int distance_town = TANK_SIZE * MPG_TOWN;
 	int distance_highway = TANK_SIZE * MPG_HIGHWAY;
-	{	cout << "Distance per single tank of gas in town: " << distance_town << " miles" << endl;
+		cout << "Distance per single tank of gas in town: " << distance_town << " miles" << endl;
 		cout << "Distance per single tank of gas on the highway: " << distance_highway << " miles" << endl;
-	}
+
 }
